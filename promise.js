0b98d6c905f13f4da0f481caa9c63addc81439b6
fararahe <faradyna.rahma@gmail.com>
2022-06-12 21:35:54 +0700

    Tantangan: Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    if(emosi == undefined || emosi == ""){
+      reject("emosi is undefined");
+    }else{
+      (async() =>{
+        let totalEmosi = 0;
+        const dataIXX = await promiseTheaterIXX();
+        const dataVGC = await promiseTheaterVGC();
+        
+        dataIXX.forEach(item => {
+          if(item.hasil == emosi){
+            totalEmosi++;
+          }
+        });
+      
+        dataVGC.forEach(item => {
+          if(item.hasil == emosi){
+            totalEmosi++;
+          }
+        });
+      
+        resolve(totalEmosi);
+      })();
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
